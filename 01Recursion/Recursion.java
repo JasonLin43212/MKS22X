00dd85bd1fba7c4894b7d11154489befeefaf5df
Jason Lin <jasonlin43212@gmail.com>
2018-02-03 22:28:38 -0500

    added fib function

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -3,7 +3,7 @@ public class Recursion{
   public static void main(String[]args){
     Recursion r = new Recursion();
     System.out.println(r.fact(6));
-
+    System.out.println(r.fib(7));
   }
 
   public int fact(int n) {
@@ -14,5 +14,19 @@ public class Recursion{
       return 1;
     }
     return n * fact(n-1);
+  }
+
+  public int fib(int n) {
+    if (n < 0) {
+      throw new IllegalArgumentException();
+    }
+    return fibHelper(n,1,0);
+  }
+
+  private int fibHelper(int n,int m, int sum){
+    if (n<1){
+      return sum;
+    }
+    return fibHelper(n-1,m+sum,m);
   }
 }
