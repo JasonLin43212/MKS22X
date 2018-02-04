e94c6131040ced864663d89460b5e70d02bd31e3
Jason Lin <jasonlin43212@gmail.com>
2018-02-03 22:42:28 -0500

    added and tested the sqrt

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -4,6 +4,7 @@ public class Recursion{
     Recursion r = new Recursion();
     System.out.println(r.fact(6));
     System.out.println(r.fib(7));
+    System.out.println(r.sqrt(3));
   }
 
   public int fact(int n) {
@@ -28,5 +29,22 @@ public class Recursion{
       return sum;
     }
     return fibHelper(n-1,m+sum,m);
+  }
+
+  public double sqrt(double n){
+    if (n<0) {
+      throw new IllegalArgumentException();
+    }
+    if (n==0){
+      return 0;
+    }
+    return sqrtHelper(n, 1);
+  }
+
+  private double sqrtHelper(double n, double guess) {
+    if (Math.abs(guess*guess-n)<0.000000001){
+      return guess;
+    }
+    return sqrtHelper(n,(n/guess+guess)/2);
   }
 }
