47081bc560e7595aa1ae3f8d2076f5a464fd5345
Jason Lin <jasonlin43212@gmail.com>
2018-02-10 11:13:00 -0500

    created and tested the solve function

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -3,15 +3,10 @@ import java.util.*;
 public class QueenBoard{
 
   public static void main(String[]args){
-    QueenBoard q = new QueenBoard(8);
-    q.addQueen(3,4);
+    QueenBoard q = new QueenBoard(4);
+    System.out.println(q.solve());
     System.out.println(q);
-    q.addQueen(2,6);
-    System.out.println(q);
-    q.removeQueen(2,3);
-    System.out.println(q);
-    q.removeQueen(3,4);
-    System.out.println(q);
+
   }
 
   private int[][] board;
@@ -84,6 +79,46 @@ public class QueenBoard{
       output += "\n";
     }
     return output;
+  }
+
+  /**
+  *@return false when the board is not solveable and leaves the board filled with zeros;
+  *        true when the board is solveable, and leaves the board in a solved state
+  *@throws IllegalStateException when the board starts with any non-zero value
+  */
+  public boolean solve(){
+    for (int r=0; r<size; r++){
+      for (int c=0; c<size; c++) {
+        if (board[r][c] != 0){
+          throw new IllegalStateException();
+        }
+      }
+    }
+    return solveHelper(0,0);
+  }
+
+  private boolean solveHelper(int row, int column){
+    if (row == size){
+      if (column == 0) {
+        return false;
+      }
+      for (int r=0; r<size; r++){
+        if (board[r][column-1] == -1){
+          removeQueen(r,column-1);
+          return solveHelper(r+1,column-1);
+        }
+      }
+    }
+    if (column == size -1){
+      if (addQueen(row,column)){
+        return true;
+      }
+      return solveHelper(row+1,column);
+    }
+    if (addQueen(row,column)){
+      return solveHelper(0,column+1);
+    }
+    return solveHelper(row+1,column);
   }
 
 }
