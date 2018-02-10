641cc30313ec0ddf08be4b3f60e42369cf40316b
Jason Lin <jasonlin43212@gmail.com>
2018-02-10 10:05:19 -0500

    added the removeQueen method and tested it

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -4,7 +4,13 @@ public class QueenBoard{
 
   public static void main(String[]args){
     QueenBoard q = new QueenBoard(8);
-
+    q.addQueen(3,4);
+    System.out.println(q);
+    q.addQueen(2,6);
+    System.out.println(q);
+    q.removeQueen(2,3);
+    System.out.println(q);
+    q.removeQueen(3,4);
     System.out.println(q);
   }
 
@@ -33,6 +39,32 @@ public class QueenBoard{
     return true;
   }
 
+  public boolean removeQueen(int r, int c){
+    if (board[r][c] != -1) {
+      return false;
+    }
+    board[r][c]++;
+     for (int i=c+1; i<size; i++){
+      board[r][i]--;
+    }
+    for (int i=1; r+i<size && c+i<size; i++){
+      board[r+i][c+i]--;
+    }
+    for (int i=1; r-i>=0 && c+i<size; i++){
+      board[r-i][c+i]--;
+    }
+    return true;
+  }
+
+  /**
+  *@return The output string formatted as follows:
+  *All numbers that represent queens are replaced with 'Q'
+  *all others are displayed as underscores '_'
+  *There are spaces between each symbol:
+  *Q _ _ _
+  *_ _ Q _
+  * etc.
+  */
   public String toString() {
     String output = "";
     for (int r=0;r<size;r++){
