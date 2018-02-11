624dca628aa36ce6a88b3dd4eeddd6fbd291ef13
Jason Lin <jasonlin43212@gmail.com>
2018-02-10 22:33:11 -0500

    removed the stackoverflow error from the solve method

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -2,13 +2,50 @@ import java.util.*;
 
 public class QueenBoard{
 
+
   public static void main(String[]args){
-    QueenBoard q = new QueenBoard(7);
-    System.out.println(q.countSolutions());
+    QueenBoard q = new QueenBoard(26);
+    //System.out.println(q.countSolutions());
     System.out.println(q.solve());
     System.out.println(q);
   }
+  /*
+  public static void main(String[] args){
+    QueenBoard b = new QueenBoard(4);
 
+    System.out.println(b.solve()); //prints true
+    System.out.println(b); //prints a valid solution
+
+    try{
+      b.solve();
+    }catch(IllegalStateException e){
+      System.out.println("Error: The board contains non-zero values");
+    } //prints "Error: The board contains non-zero values"
+
+    try{
+      b.countSolutions();
+    }catch(IllegalStateException e){
+      System.out.println("Error: The board contains non-zero values");
+    } //prints "Error: The board contains non-zero values"
+
+    for (int i = 0; i < 12; i++){
+      QueenBoard a = new QueenBoard(i);
+      System.out.println("# of Solutions for " + i + ": " + a.countSolutions());
+      /*          Expected Values
+       i --> # of Solutions   i --> # of Solutions
+      0 --> 1                      6 --> 4
+      1 --> 1                      7 --> 40
+      2 --> 0                      8 --> 92
+      3 --> 0                      9 --> 352
+      4 --> 2                    10 --> 724
+      5 --> 10                  11 --> 2680
+      */
+  /*
+      System.out.println(a); //prints out an empty i by i grid of underscores
+    }
+  }
+
+*/
   private int[][] board;
   private int size;
 
@@ -25,12 +62,15 @@ public class QueenBoard{
       return false;
     }
     board[r][c] = -1;
+    //Straight to the right
     for (int i=c+1; i<size; i++){
       board[r][i]++;
     }
+    //Diagonal down
     for (int i=1; r+i<size && c+i<size; i++){
       board[r+i][c+i]++;
     }
+    //Diagonal up
     for (int i=1; r-i>=0 && c+i<size; i++){
       board[r-i][c+i]++;
     }
@@ -42,12 +82,15 @@ public class QueenBoard{
       return false;
     }
     board[r][c]++;
-     for (int i=c+1; i<size; i++){
+    //Straigh to the right
+    for (int i=c+1; i<size; i++){
       board[r][i]--;
     }
+    //Diagonal down
     for (int i=1; r+i<size && c+i<size; i++){
       board[r+i][c+i]--;
     }
+    //Diagonal Up
     for (int i=1; r-i>=0 && c+i<size; i++){
       board[r-i][c+i]--;
     }
@@ -92,34 +135,28 @@ public class QueenBoard{
         }
       }
     }
-    return solveHelper(0,0);
+    return solveHelper(0);
   }
 
-  private boolean solveHelper(int row, int column){
-    if (row == size){
-      //Once you backtracked all the way to the beginning and still
-      //haven't found any solutions, then there are no solutions
-      if (column == 0) {
-        return false;
-      }
-      //This removes the previous queen and adds in a new one right after it
-      for (int r=0; r<size; r++){
-        if (board[r][column-1] == -1){
-          removeQueen(r,column-1);
-          return solveHelper(r+1,column-1);
-        }
-      }
-    }
-
-    //This case is for a solved board
+  private boolean solveHelper(int column){
+    //Once the column reaches beyond the board, you know it was able to add to the last column
     if (column == size){
       return true;
     }
-
-    if (addQueen(row,column)){
-      return solveHelper(0,column+1);
+    for (int row = 0; row<size; row++){
+      //Check if you can add a queen there
+      if (addQueen(row,column)){
+        //Check if the next column can be added to
+        //Once it reaches the last column, the true will carry over
+        if (solveHelper(column+1)){
+          return true;
+        }
+        //If you are not able to add to the last column from this point
+        //remove the queen you just added and try the next one
+        removeQueen(row,column);
+      }
     }
-    return solveHelper(row+1,column);
+    return false;
   }
 
   /**
