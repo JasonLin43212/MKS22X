88f3e23c7c2e6e8c0fb730a3885ab552a1dede8c
Jason Lin <jasonlin43212@gmail.com>
2018-02-22 22:36:17 -0500

    tested my knightboard using crystal's driver

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -1,16 +1,116 @@
 public class KnightBoard {
 
-  public static void main(String[]args){
-      KnightBoard k = new KnightBoard(5,5);
-      
-      //k.solve(0,0);
-      //System.out.println(k.countSolutions(0,0));
-      k.addKnight(2,3);
-      System.out.println(k.toStringMoves());
+  public static void main(String[] args){
+    KnightBoard a = new KnightBoard(3,3);
+
+    System.out.println(a);
+    /* Prints
+      _ _ _
+      _ _ _
+      _ _ _
+    */
+
+    for (int i = 0; i < 3; i++){
+      for (int j = 0; j < 3; j++){
+        if (a.solve(i,j)){
+          System.out.println("There is an error with your solve method");
+        }
+      }
+    } //prints nothing
+
+    System.out.println(a.countSolutions(0,0)); //prints 0
+
+
+
+    KnightBoard b = new KnightBoard(5,5);
+    System.out.println(b.solve(0,0)); //prints true
+    System.out.println(b); //prints a valid solution
+
+    try{
+      b.solve(0,0);
+    }catch(IllegalStateException e){
+      System.out.println("Error: The board contains non-zero values");
+    } //prints "Error: The board contains non-zero values"
+
+    try{
+      b.countSolutions(0,0);
+    }catch(IllegalStateException e){
+      System.out.println("Error: The board contains non-zero values");
+    } //prints "Error: The board contains non-zero values"
+
+    try{
+      KnightBoard b1 = new KnightBoard(-1,0);
+    }catch(IllegalArgumentException e){
+      System.out.println("Error: There cannot be negative parameters in the constructor");
+    } //prints "Error: There cannot be negative parameters in the constructor"
+
+    try{
+      KnightBoard b1 = new KnightBoard(1,-1);
+    }catch(IllegalArgumentException e){
+      System.out.println("Error: There cannot be negative parameters in the constructor");
+    } //prints "Error: There cannot be negative parameters in the constructor"
+
+    try{
+      KnightBoard b1 = new KnightBoard(-1,-1);
+    }catch(IllegalArgumentException e){
+      System.out.println("Error: There cannot be negative parameters in the constructor");
+    } //prints "Error: There cannot be negative parameters in the constructor"
+
+    try{
+      KnightBoard b1 = new KnightBoard(5,5);
+      b1.solve(0,-1);
+    }catch(IllegalArgumentException e){
+      System.out.println("Error: There cannot be negative parameters");
+    } //prints "Error: There cannot be negative parameters"
+
+    try{
+      KnightBoard b1 = new KnightBoard(5,5);
+      b1.solve(-1,0);
+    }catch(IllegalArgumentException e){
+      System.out.println("Error: There cannot be negative parameters");
+    } //prints "Error: There cannot be negative parameters"
+
+    try{
+      KnightBoard b1 = new KnightBoard(5,5);
+      b1.solve(-1,-1);
+    }catch(IllegalArgumentException e){
+      System.out.println("Error: There cannot be negative parameters");
+    } //prints "Error: There cannot be negative parameters"
+
+
+
+    for (int i = 0; i < 5; i++){
+      for (int j = 0; j < 5; j++){
+        KnightBoard abc = new KnightBoard(5,5);
+        System.out.println(abc.solve(i,j)); //prints alternating lines of true/false starting with true
+      }
+    }
+    KnightBoard c = new KnightBoard(5,5);
+
+    int totalSol = 0;
+    for (int i = 0; i < 5; i++){
+      for (int j = 0; j < 5; j++){
+        totalSol += c.countSolutions(i,j);
+      }
+    }
+
+    System.out.println(totalSol); //prints 1728
+
+    KnightBoard d = new KnightBoard(5,5);
+    System.out.println(d.countSolutions(0,0)); //prints 304
+
   }
+  /*
+  public static void main(String[]args){
+    KnightBoard k = new KnightBoard(5,5);
+      
+    //k.solve(0,0);
+    System.out.println(k.countSolutions(0,0));
+    System.out.println(k);
+  }
+  */
 
   private int[][] board;
-  private int[][] validMoves;
   private int row;
   private int col;
   private int[][] knightMoves = {{1,2},{2,1},{2,-1},{1,-2},{-1,-2},{-2,-1},{-2,1},{-1,2}};
@@ -20,21 +120,8 @@ public class KnightBoard {
 	    throw new IllegalArgumentException();
     }
     board = new int[startingRows][startingCols];
-    validMoves = new int[startingRows][startingCols];
     row = startingRows;
     col = startingCols;
-    //New stuff
-    for (int r=0; r<row; r++){
-	for (int c=0; c<col; c++){
-	     for (int i=0; i<8; i++){
-		 int newRow = r + knightMoves[i][0];
-		 int newCol = c + knightMoves[i][1];
-		 if (!isOutOfRange(newRow,newCol)){
-		     validMoves[r][c]++;
-		 }
-	     }
-	}
-    }
   }
 
   public String toString(){
@@ -55,18 +142,7 @@ public class KnightBoard {
     }
     return output;
   }
-
-  public String toStringMoves(){
-      String output = "";
-      for (int i=0; i<row; i++){
-	  for (int j=0; j<col; j++){
-	      output += validMoves[i][j] + " ";
-	  }
-	  output += "\n";
-      }
-      return output;
-  }
-
+  
   public boolean solve(int startingRow, int startingCol){
     if (isOutOfRange(startingRow,startingCol)) {
       throw new IllegalArgumentException();
@@ -87,10 +163,10 @@ public class KnightBoard {
       return true;
     }
     /*
-    System.out.println(Text.CLEAR_SCREEN);
-    System.out.println(Text.go(1,1));
-    System.out.println(this);
-    Text.wait(2000); //adjust this delay
+      System.out.println(Text.CLEAR_SCREEN);
+      System.out.println(Text.go(1,1));
+      System.out.println(this);
+      Text.wait(2000); //adjust this delay
     */
     for (int i=0; i<8; i++){
       int newRow = row + knightMoves[i][0];
@@ -141,48 +217,4 @@ public class KnightBoard {
   private boolean isOutOfRange(int row, int col){
     return row < 0 || row >= this.row || col < 0 || col >= this.col;
   }
-
-  public boolean solveFast (int startingRow, int startingCol){
-    if (isOutOfRange(startingRow,startingCol)) {
-      throw new IllegalArgumentException();
-    }
-    for (int r = 0; r<row; r++){
-      for (int c = 0; c<col; c++){
-        if (board[r][c] != 0){
-          throw new IllegalStateException();
-        }
-      }
-    }
-    return solveFastHelper(startingRow, startingCol, 1);
-  }
-
-    private boolean solveFastHelper(int row, int col, int level){
-	board[row][col] = level;
-	if (level == this.row * this.col){
-	    return true;
-	}
-	int[] possibleMoves;
-	for (int i=0; i<8; i++){
-	    int newRow = row + knightMoves[i][0];
-	    int newCol = col + knightMoves[i][1];
-	    if (!isOutOfRange(newRow,newCol) && board[newRow][newCol] == 0 ){
-		if (solveHelper(newRow, newCol, level+1)){
-		    return true;
-		}
-		board[newRow][newCol] = 0;
-	    }
-	}
-	board[row][col] = 0;
-	return false;
-    }
-    
-    public void addKnight(int row, int col){
-	for (int i=0; i<8; i++){
-	    int newRow = row + knightMoves[i][0];
-	    int newCol = col + knightMoves[i][1];
-	    if (!isOutOfRange(newRow,newCol) && board[newRow][newCol] == 0 ){
-		validMoves[newRow][newCol]--;
-	    }
-	}
-    }
 }
