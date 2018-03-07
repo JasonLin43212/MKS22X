177cf5c3cc4f0721f27b28f4240006544fea3bf1
Jason Lin <jasonlin43212@gmail.com>
2018-03-07 14:42:28 -0500

    made the stomp method

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -14,19 +14,47 @@ public class USACO {
 	try {
 	    File f = new File(filename);
 	    Scanner in = new Scanner(f);
-	    row = in.next();
-	    col = in.next();
-	    elevation = in.next();
-	    numInstructions = in.next();
+	    row = Integer.parseInt(in.next());
+	    col = Integer.parseInt(in.next());
+	    elevation = Integer.parseInt(in.next());
+	    numInstructions = Integer.parseInt(in.next());
 	    data = new int[row][col];
 	    for (int i=0; i<row; i++){
-		for (int j=0; j<c
+		for (int j=0; j<col; j++){
+		    data[i][j] = Integer.parseInt(in.next());
+		}
 	    }
+	    for (int i=0; i<numInstructions; i++){
+		stomp(Integer.parseInt(in.next())-1,
+		      Integer.parseInt(in.next())-1,
+		      Integer.parseInt(in.next()),
+		      data);
+	    }
+	    System.out.println(Arrays.deepToString(data));
 	}catch (FileNotFoundException e){
 	    System.out.println("File is not found");
 	    System.exit(1);
 	}
-	return 1;
+	return 0;
+    }
 
+    private static void stomp(int r, int c, int depth, int[][] data){
+	int max = 0;
+	for (int i=r; i<r+3; i++){
+	    for (int j=c; j<c+3; j++){
+		int current = data[i][j];
+		if (max < current){
+		    max = current;
+		}
+	    }
+	}
+	int newDepth = max - depth;
+	for (int i=r; i<r+3; i++){
+	    for (int j=c; j<c+3; j++){
+		if (data[i][j] > newDepth){
+		    data[i][j] = newDepth;
+		}
+	    }
+	}
     }
 }
