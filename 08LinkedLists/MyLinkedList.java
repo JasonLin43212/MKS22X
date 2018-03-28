114b26118c2b345db821a3c4ab3d43b650fe48ad
Jason Lin <jasonlin43212@gmail.com>
2018-03-28 14:39:37 -0400

    rearranged order

--- 08LinkedLists/MyLinkedList.java
+++ 08LinkedLists/MyLinkedList.java
@@ -9,28 +9,6 @@ public class MyLinkedList {
     
   }
 
-  private class Node {
-
-    private Node next,prev;
-    private int data;
-
-    public Node (int value) {
-      data = value;
-      next = null;
-      prev = null;
-    }
-
-    public Node getNext() {return next;}
-    public Node getPrev() {return prev;}
-    public int getValue() {return data;}
-
-    public String toString() {return data + "";}
-
-    public void setNext(Node newNext) {next = newNext;}
-    public void setPrev(Node newPrev) {prev = newPrev;}
-    public void setValue(int newValue) {data = newValue;}
-  }
-
   private Node first,last;
   private int length;
     private boolean canPrint;
@@ -104,4 +82,26 @@ public class MyLinkedList {
     public void setPrint(boolean value) {
 	canPrint = value;
     }
+
+    private class Node {
+
+    private Node next,prev;
+    private int data;
+
+    public Node (int value) {
+      data = value;
+      next = null;
+      prev = null;
+    }
+
+    public Node getNext() {return next;}
+    public Node getPrev() {return prev;}
+    public int getValue() {return data;}
+
+    public String toString() {return data + "";}
+
+    public void setNext(Node newNext) {next = newNext;}
+    public void setPrev(Node newPrev) {prev = newPrev;}
+    public void setValue(int newValue) {data = newValue;}
+  }
 }
