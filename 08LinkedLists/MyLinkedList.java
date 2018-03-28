a07ee3356ab75eeffad645a60de664602be31332
Jason Lin <jasonlin43212@gmail.com>
2018-03-28 00:15:35 -0400

    added some linked list methods

--- 08LinkedLists/MyLinkedList.java
+++ 08LinkedLists/MyLinkedList.java
@@ -1,25 +1,72 @@
 public class MyLinkedList {
 
-  private class Node {
+  private Node first,last;
+  private int length;
 
-    private Node next,prev;
-    private int data;
-
-    public Node (int value) {
-      data = value;
-      next = null;
-      prev = null;
-    }
-
-    public Node getNext() {return next;}
-    public Node getPrev() {return prev;}
-    public int getValue() {return data;}
-
-    public String toString() {return data;}
-
-    public void setNext(Node newNext) {next = newNext;}
-    public void setPrev(Node newPrev) {prev = newPrev;}
-    public void setValue(int newValue) {data = newValue;}
+  public MyLinkedList() {
+    length = 0;
   }
 
+  public String toString() {
+    String output = "[";
+    Node current = first;
+    for (int i=0; i<length; i++){
+      output += current.getValue;
+      if (i != length-1){
+        output += ",";
+      }
+    }
+    return output + "]";
+  }
+
+  public int size() {
+    return length;
+  }
+
+  public int get(int index) {
+    if (index >= length || index < 0) {
+      throw new IndexOutOfBoundsException();
+      System.exit(1);
+    }
+    Node current = first;
+    for (int i=0; i<index; i++){
+      current = current.getNext();
+    }
+    return current.getValue();
+  }
+
+  public void set(int index, int newValue) {
+    if (index >= length || index < 0) {
+      throw new IndexOutOfBoundsException();
+      System.exit(1);
+    }
+    Node current = first;
+    for (int i=0; i<index; i++){
+      current = current.getNext();
+    }
+    current.setValue(newValue);
+  }
+
+}
+
+private class Node {
+
+  private Node next,prev;
+  private int data;
+
+  public Node (int value) {
+    data = value;
+    next = null;
+    prev = null;
+  }
+
+  public Node getNext() {return next;}
+  public Node getPrev() {return prev;}
+  public int getValue() {return data;}
+
+  public String toString() {return data;}
+
+  public void setNext(Node newNext) {next = newNext;}
+  public void setPrev(Node newPrev) {prev = newPrev;}
+  public void setValue(int newValue) {data = newValue;}
 }
