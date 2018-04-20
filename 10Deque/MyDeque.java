94476f69641262b2d2e6b21e06d55c88dd388a35
Jason Lin <jasonlin43212@gmail.com>
2018-04-19 20:20:23 -0400

    got resize to work

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -1,18 +1,29 @@
+import java.util.*;
+
 public class MyDeque<E>{
 
   public static void main(String[]args){
-    MyDeque d = new MyDeque();
-    System.out.println(d.size());
+    MyDeque<Integer> d = new MyDeque<>();
+    d.addLast(23);
+    d.addLast(32);
+    d.addLast(232);
+    d.addLast(23);
+    d.addLast(32);
+    d.addLast(232);
+    d.addLast(23);
+    d.addLast(32);
+    d.addLast(232);
+    d.addLast(23);
+    d.addLast(32);
+    d.addLast(232);
+    System.out.println(d);
   }
 
-
   public E[] data;
-  public int size;
+  public int size,front,back;
 
-  @SuppressWarnings("unchecked")
   public MyDeque() {
-    data = (E[]) new Object[10];
-    size = 0;
+    this(10);
   }
 
   @SuppressWarnings("unchecked")
@@ -22,10 +33,54 @@ public class MyDeque<E>{
     }
     data = (E[]) new Object[initialCapacity];
     size = 0;
+    front = 0;
+    back = 0;
+  }
+
+  public String toString() {
+    /*
+    String out = "[";
+    for (int i=0; i<size; i++){
+      out += data[(front+i)%data.length];
+      if (i!=size-1){
+        out += ",";
+      }
+    }
+    return out+"]";
+    */
+    return Arrays.toString(data);
   }
 
   public int size() {
     return size;
+  }
+
+  @SuppressWarnings("unchecked")
+  public void resize() {
+    E[] newArr = (E[])new Object[size*2];
+    for (int i=0; i<data.length; i++){
+      newArr[i] = data[(i+front)%data.length];
+    }
+    data = newArr;
+  }
+
+  public void addLast(E value) {
+    if (size == data.length){
+      resize();
+    }
+    if (size != 0){
+      back = (back+1) % data.length;
+    }
+    data[back] = value;
+    size++;
+  }
+
+  public E removeLast() {
+    E cur = data[back];
+    data[back] = null;
+    back--;
+    size--;
+    return cur;
   }
 
 }
