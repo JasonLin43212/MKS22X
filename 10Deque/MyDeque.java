ca384229594f5b38de2784321b6d7fcbcc465878
Jason Lin <jasonlin43212@gmail.com>
2018-04-21 05:48:02 -0400

    tested using crystal's driver

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -2,13 +2,51 @@ import java.util.*;
 
 public class MyDeque<E>{
 
-  public static void main(String[]args){
-    MyDeque<Integer> d = new MyDeque<>(4);
-    d.addFirst(12);
-    System.out.println(d);
-    System.out.println(d.getFirst());
-    System.out.println(d.getLast());
-    System.out.println(d);
+  public static void main(String[] args) {
+    MyDeque<String> a = new MyDeque<>(), a1 = new MyDeque<>();
+    ArrayList<String> b = new ArrayList<>();
+
+    int size = Integer.parseInt(args[0]);
+    for(int i = 0; i < size; i++){
+      int temp = (int)(Math.random() * 1000);
+      if(temp % 2 == 0){
+        a.addFirst("" + temp);
+        a1.addFirst("" + temp);
+        b.add(0, "" + temp);
+      }
+      else{
+        a.addLast("" + temp);
+        a1.addLast("" + temp);
+        b.add("" + temp);
+      }
+    }
+
+    int index = 0;
+    boolean hasError = false;
+    String errorEvaluation = "Errors found at these indices: ";
+    for (String x : b){
+      if (!(x.equals(a.getFirst()))){
+        System.out.println("The getFirst() function is incorrect at index " + index);
+        hasError = true;
+      }
+      if (!(x.equals(a.removeFirst()))){
+        System.out.println("There is an error at index " + index);
+        errorEvaluation += index + ", ";
+        hasError = true;
+      }
+      index++;
+    }
+
+
+    if(hasError){
+      errorEvaluation = errorEvaluation.substring(0, errorEvaluation.length() - 2);
+      System.out.println(errorEvaluation);
+      System.out.println("MyDeque: " + a1);
+      System.out.println("Actual Deque: " + b);
+    }
+    else{
+      System.out.println("Your deque is bug-free!");
+    }
   }
 
   public E[] data;
